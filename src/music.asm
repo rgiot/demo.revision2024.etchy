72f1c43989e9a9b08c201778973e152f64eb67df
Romain Giot <giot.romain@gmail.com>
2024-03-03 22:29:42 +0100

    update music

--- src/music.asm
+++ src/music.asm
@@ -1,9 +1,11 @@
 PLY_AKM_HARDWARE_CPC = 1
 ;PLY_AKM_MANAGE_SOUND_EFFECTS = 1
 
-	include "./music/krocket2_akm_playerconfig.asm"
+	;include "./music/krocket2_akm_playerconfig.asm"
+	include "./music/lookool_akm_playerconfig.asm"
 music_data
-	include "./music/krocket2_akm.asm"
+	;include "./music/krocket2_akm.asm"
+	include "./music/lookool_akm.asm"
 music_player
 	include "./music/PlayerAkm.asm"
 

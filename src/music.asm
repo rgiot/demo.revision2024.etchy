37e077e6254783c01bedd8dba45cb5f8e9c4b799
Romain Giot <giot.romain@gmail.com>
2024-03-04 21:14:46 +0100

    Use v2 of the music

--- src/music.asm
+++ src/music.asm
@@ -2,12 +2,12 @@ PLY_AKM_HARDWARE_CPC = 1
 ;PLY_AKM_MANAGE_SOUND_EFFECTS = 1
 
 	;include "./music/krocket2_akm_playerconfig.asm"
-	include "./music/lookool_akm_playerconfig.asm"
+	include "./music/lookool2_akm_playerconfig.asm"
 music_data
 	;include "./music/krocket2_akm.asm"
-	include "./music/lookool_akm.asm"
+	include "./music/lookool2_akm.asm"
 music_player
-	include "./music/PlayerAkm.asm"
+	include "./music/PlayerAkm_basm.asm"
 
 	assert PLY_AKM_USE_HOOKS == 0
 	assert PLY_AKM_STOP_SOUNDS == 0
